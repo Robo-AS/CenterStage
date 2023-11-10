cfe308fd81e9b878fd787beac11f6030607ced9a
daria_lazar <lazardaria08@gmail.com>
2023-11-10 19:49:18 +0200

    Sters functie setDirection, setat rotile din spate sa se miste in directie inversa.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UsefulFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UsefulFunctions.java
@@ -17,15 +17,18 @@ public class UsefulFunctions extends LinearOpMode {
 
         setMotorModes(DcMotor.RunMode.RUN_USING_ENCODERS);
 
+
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+
+        //telling the motors which direction to take(backwords or forward)
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     //
@@ -48,18 +51,26 @@ public class UsefulFunctions extends LinearOpMode {
     public void TeleOpDrive() {
         setMotorModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        double x = gamepad1.left_stick_x;
-        double y = gamepad1.left_stick_y;
-        double rotation = gamepad1.right_stick_x;
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
 
-        double power_fl = x + y - rotation;
-        double power_fr = -x -y - rotation;
-        double power_bl = -x + y - rotation;
-        double power_br = x - y - rotation;
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double frontRightPower = ( -y + x - rx) / denominator;
+        double backLeftPower = ( -y + x + rx) / denominator;
+        double backRightPower = ( y + x - rx) / denominator;
 
-        MotorValues motorValues = new MotorValues(power_fl, power_fr, power_bl, power_br);
+        MotorValues motorValues= new MotorValues(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
 
         if (gamepad1.left_bumper) motorValues.SlowMode();
+
+        telemetry.addData("FrontLeft", frontLeftPower);
+        telemetry.addData("FrontRight", frontRightPower);
+        telemetry.addData("BackLeft", backLeftPower);
+        telemetry.addData("BackRight", backRightPower);
+
+        telemetry.update();
 
         motorValues.NormaliseValues();
         applyMotorValues(motorValues);
