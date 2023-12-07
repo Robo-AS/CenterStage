da2da0318b297cbd728811b6de8d8dd066d306a4
DVDTSB <david15tache@gmail.com>
2023-12-07 21:34:57 +0200

    vf bghthnghn tth

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -10,6 +10,8 @@ import java.util.List;
 @TeleOp(name = "TeleOpMode", group = "Linear OpMode")
 public class TeleOpMode extends MovementFunctions {
 
+    int[] angles = {180,210,240,270};
+    int[] distances = {0,0,0,0};
     Gamepad lastgamepad1=new Gamepad();
     Gamepad lastgamepad2=new Gamepad();
     enum modes{
@@ -18,6 +20,7 @@ public class TeleOpMode extends MovementFunctions {
     }
 
     modes mode = modes.MOVE;
+
 
 
     @Override
@@ -30,17 +33,38 @@ public class TeleOpMode extends MovementFunctions {
         while (opModeIsActive()) {
 
             if(mode==modes.MOVE){
+                telemetry.addLine("Move Mode");
                 teleOpDriveRelative();
 
             }else if(mode==modes.PLACE){
+                telemetry.addLine("Place Mode");
+
+                boolean[] dpad = {gamepad1.dpad_up, gamepad1.dpad_right, gamepad1.dpad_down, gamepad1.dpad_left};
+
+                for(int i=0;i<4;i++){
+                    if(dpad[i]){
+                        armCircularMovement(0.1, angles[i]);
+                    }
+                }
+
 
             }
 
-            if(gamepad1.x && lastgamepad1.x==false){
-                mode = mode==modes.MOVE ? modes.PLACE : modes.MOVE;
+            if(gamepad1.x && !lastgamepad1.x){
+                if(mode==modes.MOVE){
+                    mode = modes.PLACE;
+                }else{
+                    mode=modes.MOVE;
+                }
+
             }
-            lastgamepad1 = gamepad1;
+            telemetry.addData("x1",gamepad1.x);
+            telemetry.addData("x2",lastgamepad1.x);
+
+
             lastgamepad2 = gamepad2;
+
+            telemetry.update();
         }
     }
 }
