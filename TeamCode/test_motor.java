79bb96ec7b283314acd5ee8da295848828191985
daria_lazar <lazardaria08@gmail.com>
2024-01-05 13:42:40 +0200

    dsadsa

---
+++ TeamCode/test_motor.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.checkerframework.checker.units.qual.A;
+
+@TeleOp(name ="test_motor", group = "Linear OpMode")
+public class test_motor extends Movement Funtionsd{
+    @Override
+    public void runOpMode(){
+        initialiseArm();
+    }
+}
